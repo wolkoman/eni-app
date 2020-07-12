2b013bc1bd0245840af9d417ad769f09f0dd1d7d
Manuel Wolkowitsch <superwolko@gmail.com>
2020-07-12 20:46:07 +0200

    Add routing

--- src/App.js
+++ src/App.js
@@ -6,19 +6,33 @@ import Churches from './Churches';
 import Box from './Box';
 import Events from './Events';
 import Footer from './Footer';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 
 function App() {
   return (
-  <StyleRoot>
-      <div style={{...style.responsive, background: style.light, minHeight: '100vh', paddingBottom: 100}}>
-        <Navbar></Navbar>
-        <Box label="Pfarren">
-          <Churches></Churches>
-        </Box>
-        <Box label="Termine">
-          <Events></Events>
-        </Box>
-        <Footer></Footer>
+    <StyleRoot>
+      <div style={{...style.responsive, background: style.light, minHeight: 'calc(100vh - 120px)', paddingTop: 20, paddingBottom: 100}}>
+        <Router>
+          <Navbar></Navbar>
+          <Switch>
+            <Route exact path="/">
+              <Box label="Pfarren">
+                <Churches></Churches>
+              </Box>
+              <Box label="Termine">
+                <Events></Events>
+              </Box>
+              <Footer></Footer>
+            </Route>
+            <Route path="*">
+              <b>404</b> Seite nicht gefunden
+            </Route>
+          </Switch>
+        </Router>
       </div>
     </StyleRoot>
     );
