97ddfad385d53af80479436f5458ba0df14d81bb
Manuel Wolkowitsch <superwolko@gmail.com>
2020-07-13 08:11:29 +0200

    Refactor to article rendering to react components

--- src/Article.js
+++ src/Article.js
@@ -2,39 +2,39 @@ import React, {useEffect, useState} from 'react';
 import Radium from 'radium';
 //import {style} from './style';
 import ContentfulClient from './contentful';
-import { documentToHtmlString } from '@contentful/rich-text-html-renderer';
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 import { BLOCKS, MARKS } from '@contentful/rich-text-types';
 import { style } from './style';
+import Loader from './graphics/Loader';
 
 const Article = Radium (({ id }) => {
 
     const [entry, setEntry] = useState();
-    const [content, setContent] = useState();
+    const [component, setComponent] = useState();
     useEffect(() => {
         ContentfulClient.getEntry(id).then(entry => {
             setEntry(entry);
             console.log(entry);
-            return documentToHtmlString(entry.fields.inhalt, {
+            setComponent(documentToReactComponents(entry.fields.inhalt, {
                 renderNode: {
                     [BLOCKS.EMBEDDED_ASSET]: ({ data: { target: { fields }}}) =>
-                    `<img src="${fields.file.url}" style="max-width: 100%" alt="${fields.description}"/>`,
-                    [MARKS.]: ({ data: { target: { fields }}}) =>
+                    <img src="${fields.file.url}" style="max-width: 100%" alt="${fields.description}"/>,
+                    [BLOCKS.EMBEDDED_ASSET]: ({ data: { target: { fields }}}) =>
                     `<img src="${fields.file.url}" style="max-width: 100%" alt="${fields.description}"/>`,
                 },
-            });
-        }).then(html => {
-            setContent(html);
-            console.log(html);
+            }));
         });
     }, [id]);
 
     return <div style={{padding: 40}}>
-        <h1 style={{
-            ...style.serif,
-            paddingBottom: 5,
-            borderBottom: '1px solid #ddd',
-            }}>{entry?.fields.title}</h1>
-        <div dangerouslySetInnerHTML={{__html: content}} style={{...style.serif}}></div>
+        {component ? [
+            <h1 style={{
+                ...style.serif,
+                paddingBottom: 5,
+                borderBottom: '1px solid #ddd',
+            }}>{entry?.fields.title}</h1>,
+            component
+        ] : <Loader></Loader>}
     </div>;
 });
 
