c9c0f5a35a42cf0c0a464c9730d91dc0bedea337
Manuel Wolkowitsch <superwolko@gmail.com>
2020-07-13 09:06:24 +0200

    Add repository and feedback to footer

--- src/Footer.js
+++ src/Footer.js
@@ -6,14 +6,19 @@ const Footer = Radium(() => {
     return <div style={{textAlign: 'center', marginTop: '60px'}}>
         {[
             <FooterItem link="/impressum" title="Impressum"></FooterItem>,
+            <FooterItem link="//github.com/wolkoman/eni-app" title="Quellcode" hard={true}></FooterItem>,
+            <FooterItem title="Feedback" hard={true} onClick={() => window.open("//docs.google.com/forms/d/e/1FAIpQLSfcVTLOylH9uUk50iKZvXe6WiDRY-JXV5HnfhYIPR1XOFmJPA/viewform?usp=sf_link")}></FooterItem>,
         ].map((item,index) => [index === 0 ? null : <FooterSeperator></FooterSeperator> ,item]).flat()}
     </div>
 });
 
-const FooterItem = Radium(({link, title}) => {
-    return <Link to={link} style={{color: 'grey', textDecoration: 'none'}}>{title}</Link>
+const FooterItem = Radium(({link, title, hard = false, onClick}) => {
+    return hard
+    ? <a href={link} onClick={onClick} style={{color: 'grey', textDecoration: 'none', cursor: 'pointer'}}>{title}</a>
+    : <Link to={link} style={{color: 'grey', textDecoration: 'none'}}>{title}</Link>
+    ;
 });
-const FooterSeperator = Radium(({link, title}) => {
+const FooterSeperator = Radium(() => {
     return <span style={{display: 'inline-block', margin: '0 10px', color: 'grey'}}>-</span>
 });
 
