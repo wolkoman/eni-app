739a43dce36356042242463e7933d3fe417f11b7
Manuel Wolkowitsch <superwolko@gmail.com>
2020-11-07 19:25:50 +0100

    Add persistence to store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,6 +1,34 @@
 import { applyMiddleware, combineReducers, createStore } from "redux";
 import thunk from "redux-thunk";
 import { authReducer } from "./auth.reducer";
+import { State } from "./state";
+
+export const loadState = () => {
+  try {
+    const serializedState = localStorage.getItem("state");
+    if (serializedState === null) {
+      return undefined;
+    }
+    return JSON.parse(serializedState);
+  } catch (err) {
+    return undefined;
+  }
+};
+
+export const saveState = (state: State) => {
+  try {
+    const serializedState = JSON.stringify(state);
+    localStorage.setItem("state", serializedState);
+  } catch {}
+};
 
 const rootReducer = combineReducers({ auth: authReducer as any });
-export const store = createStore(rootReducer, applyMiddleware(thunk));
+export const store = createStore(
+  rootReducer,
+  loadState(),
+  applyMiddleware(thunk)
+);
+
+store.subscribe(() => {
+  saveState(store.getState() as State);
+});
