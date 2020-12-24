6d39005f9aab79bd994b62a21fa689c5d299f85c
Manuel Wolkowitsch <superwolko@gmail.com>
2020-12-24 01:06:33 +0100

    Support galleries

--- src/components/Article.tsx
+++ src/components/Article.tsx
@@ -2,31 +2,39 @@ import React, { useEffect, useState } from "react";
 import Radium from "radium";
 import { style } from "../util/style";
 import Loader from "../Graphic/Loader";
+import cockpit, { host } from "../util/cockpit";
 
 interface Article {
   title: string;
   content: string;
-  layout?: (TextSection)[]
-  author?: string
+  layout?: TextSection[];
+  author?: string;
 }
 interface TextSection {
   component: "text";
-  settings: {text: string}
+  settings: { text: string };
 }
 const Article = Radium(({ article }: { article: () => Promise<Article> }) => {
   const [object, setObject] = useState<Article>();
   const [error, setError] = useState(false);
   useEffect(() => {
-    article().then(setObject).catch(() => setError(true));
+    article()
+      .then(setObject)
+      .catch(() => setError(true));
   }, [article]);
 
   return (
-    <div style={{ padding: style.padding }}>
-      {error ? <div style={{fontStyle: "italic"}}>Dieser Artikel existiert nicht.</div> : object ? (
+    <div style={{ padding: style.padding, [style.mobile]: { padding: 0 } }}>
+      {error ? (
+        <div style={{ fontStyle: "italic" }}>
+          Dieser Artikel existiert nicht.
+        </div>
+      ) : object ? (
         <div>
           <div
             style={{
-            borderBottom: "1px solid #ddd", }}
+              borderBottom: "1px solid #ddd",
+            }}
             key="title"
           >
             <h1
@@ -37,23 +45,67 @@ const Article = Radium(({ article }: { article: () => Promise<Article> }) => {
             >
               {object.title}
             </h1>
-              { object.author ? <div style={{marginBottom: 20, ...style.serif, fontStyle: "italic"}}>von {object.author}</div> : null}
+            {object.author ? (
+              <div
+                style={{
+                  marginBottom: 20,
+                  ...style.serif,
+                  fontStyle: "italic",
+                }}
+              >
+                von {object.author}
+              </div>
+            ) : null}
           </div>
-          { object.layout ? object.layout.map((l,index) => <div
-            key={index}
-            style={{ overflowWrap: "break-word", lineHeight: 1.5 }}
-            dangerouslySetInnerHTML={{__html: l.settings.text}}
-        />) : <div
-            key="content"
-            style={{ overflowWrap: "break-word", lineHeight: 1.5 }}
-            dangerouslySetInnerHTML={{ __html: object.content }}
-          ></div>}
-          </div>
+          {object.layout ? (
+            object.layout.map((l, index) => (
+              <LayoutComponent layout={l} key={index} />
+            ))
+          ) : (
+            <div
+              key="content"
+              style={{ overflowWrap: "break-word", lineHeight: 1.5 }}
+              dangerouslySetInnerHTML={{ __html: object.content }}
+            ></div>
+          )}
+        </div>
       ) : (
         <Loader></Loader>
       )}
     </div>
   );
+});
+
+const LayoutComponent = Radium(({ layout }: { layout: any }) => {
+  return {
+    text: (
+      <div
+        style={{ overflowWrap: "break-word", lineHeight: 1.5 }}
+        dangerouslySetInnerHTML={{ __html: layout.settings.text }}
+      />
+    ),
+    gallery: (
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "1fr",
+          gridGap: 10,
+        }}
+      >
+        {layout.settings.gallery?.map((entry: any) => (
+          <div style={{ marginBottom: 10 }}>
+            <img
+              src={`${host}${entry.path}`}
+              style={{ width: "100%", maxWidth: 800 }}
+            />
+            <div style={{ padding: 5, background: "#ddd" }}>
+              {entry.meta.title}
+            </div>
+          </div>
+        ))}
+      </div>
+    ),
+  }[layout.component as "text" | "gallery"];
 });
 
 export default Article;
