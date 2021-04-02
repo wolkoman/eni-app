c8ba6ec808a61d1a3cecbad4d418d83ebe660a85
Manuel Wolkowitsch <superwolko@gmail.com>
2021-04-02 14:45:40 +0200

    Revert "Insert Karfreitag"
    This reverts commit 29e310dcacad7992e644e15691b57fc7d37d2a09.

--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -1,60 +1,24 @@
-import React from 'react';
-import Churches from '../../components/Churches';
-import Title from './Title';
-import Box from '../../components/Box';
-
-enum Ort{
-  EMMAUS = "Pfarre Emmaus am Wienerberg",
-  NIKOLAUS = "Pfarre Inzersdorf - St. Nikolaus",
-  NEUSTIFT = "Pfarre Inzersdorf-Neustift",
-}
+import React from "react";
+import Announcement from "../../components/Announcement";
+import VaticanNews from "../../components/VaticanNews";
+import Churches from "../../components/Churches";
+import Events from "./Events/Events";
+import Instagram from "../../components/Instagram";
+import Livestreams from "./Livestreams/Livestreams";
+import Title from "./Title";
+import InformationPanel from "../../components/InformationPanel";
+import NewsletterSubscription from "../../components/NewsletterSubscription";
 
 export default () => (
   <div>
-    <Title/>
-    <Churches/>
-    <Box label="Termine" padded={true} styled={true}>
-      <div style={{ fontSize: 18, fontWeight: "bold", marginBottom: 10, marginTop: 15}}>Karfreitag</div>
-      <Event time="14:30 Uhr" label="Die sieben Worte Jesu am Kreuz" ort={Ort.NEUSTIFT}/>
-      <Event time="15:00 Uhr" label="Gedenken der Todesstunde Jesu (Kreuzwegandacht)" ort={Ort.NIKOLAUS}/>
-      <Event time="15:00 Uhr" label="7 Worte Jesu am Kreuz/Pfr. Dr. Brezovski" ort={Ort.EMMAUS}/>
-      <Event time="16:00 Uhr" label="Offene Kirche mit der Möglicheit des stillen Gedenkens an Jesu" ort={Ort.NIKOLAUS}/>
-      <Event time="16:00 Uhr" label="Karfreitagsliturgie für Kinder" ort={Ort.NEUSTIFT}/>
-      <Event time="18:00 Uhr" label="Karfreitagsliturgie/Pfr. Dr. Brezovski" ort={Ort.EMMAUS}/>
-      <Event time="18:00 Uhr" label="Karfreitagsliturgie/P. Alois" ort={Ort.NEUSTIFT}/>
-      <Event time="19:00 Uhr" label="Karfreitagsliturgie/Oliver Meidl" ort={Ort.NIKOLAUS}/>
-
-      <div style={{ fontSize: 18, fontWeight: "bold", marginBottom: 10, marginTop: 15}}>Karsamstag</div>
-      <Event time="08:00 Uhr" label="Laudes/Pfr. Dr. Brezovski" ort={Ort.EMMAUS}/>
-      <Event time="08:30 Uhr" label="Grabwache (bis 16:00 Uhr)" ort={Ort.EMMAUS}/>
-      <Event time="16:00 Uhr" label="Speisensegnung/Pfr. Dr. Brezovski" ort={Ort.NIKOLAUS}/>
-      <Event time="16:00 Uhr" label="Feier der Grabesruhe für Kinder" ort={Ort.NEUSTIFT}/>
-      <Event time="17:00 Uhr" label="Speisensegnung/Pfr. Dr. Brezovski" ort={Ort.EMMAUS}/>
-      <Event time="17:00 Uhr" label="Speisensegnung" ort={Ort.NEUSTIFT}/>
-      <Event time="21:00 Uhr" label="Osternachtfeier mit P. Alois SDB" ort={Ort.NEUSTIFT}/>
-      <Event time="21:00 Uhr" label="Auferstehungsfeier/Pfr. Dr. Brezovski" ort={Ort.NIKOLAUS}/>
-
-      <div style={{ fontSize: 18, fontWeight: "bold", marginBottom: 10, marginTop: 15}}>Ostersonntag</div>
-      <Event time="05:00 Uhr" label="Auferstehungsfeier/Pfr. Dr. Brezovski" ort={Ort.EMMAUS}/>
-      <Event time="09:00 Uhr" label="Hl. Messe/Kpl. Gil" ort={Ort.NIKOLAUS}/>
-      <Event time="10:00 Uhr" label="Hl. Messe mit P. Hermann SDB (bitte um Anmeldung in Kanzlei)" ort={Ort.NEUSTIFT}/>
-      <Event time="10:30 Uhr" label="Hl. Messe/Pfr. Dr. Brezovski" ort={Ort.EMMAUS}/>
-
-      <div style={{ fontSize: 18, fontWeight: "bold", marginBottom: 10, marginTop: 15}}>Ostermontag</div>
-      <Event time="10:00 Uhr" label="Hl. Messe/P. Josef SDB (bitte um Anmeldung in Kanzlei)" ort={Ort.NEUSTIFT}/>
-      <Event time="10:30 Uhr" label="Hl. Messe/Pfr. Dr. Brezovski" ort={Ort.EMMAUS}/>
-    </Box>
+    <Title></Title>
+    <Livestreams />
+    <Churches />
+    <Events />
+    <Announcement />
+    <Instagram />
+    <NewsletterSubscription />
+    <VaticanNews />
+    <InformationPanel />
   </div>
 );
-
-function Event({time, label, ort}: { time: string, label: string, ort: Ort }) {
-  return <div style={{display: "flex", marginBottom: 10}}>
-    <div style={{width: 100, fontSize: 18, flexShrink: 0, flexGrow: 0}}>
-      {time}
-    </div>
-    <div style={{flexShrink: 0, flexGrow: 0}}>
-      <span style={{fontSize: 18}}>{label}</span>
-      <br/>{ort}
-    </div>
-  </div>
-}
