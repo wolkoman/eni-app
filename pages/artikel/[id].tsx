1e6f5c78f4e0ec1e8397ab64a0af1bdbdbd3ead9
Manuel Wolkowitsch <superwolko@gmail.com>
2021-06-17 21:37:41 +0200

    Remove fail

--- pages/artikel/[id].tsx
+++ pages/artikel/[id].tsx
@@ -1,24 +1,9 @@
 import Site from '../../components/Site';
-import React, {useEffect} from 'react';
-import {Cockpit, CockpitArticle, cockpitBase} from '../../util/cockpit';
-import {SanitizeHTML} from '../../components/SanitizeHtml';
+import React from 'react';
+import {Cockpit} from '../../util/cockpit';
 
-export default function Article({article}: {article: CockpitArticle}) {
-  useEffect(() => {if(article.external_url) {
-    window.location.replace(article.external_url);
-  }},[]);
-  return <Site narrow={true}>
-    <div className="flex flex-col mt-12 mb-6">
-        <div className="font-bold text-5xl">{article.title}</div>
-      <div>am <div className="inline font-bold text-lg">{new Date(article._created * 1000).toLocaleDateString()}</div> von <div className="inline font-bold">{article.author}</div></div>
-    </div>
-    <img src={`${cockpitBase}/${article.preview_image.path}`} className="h-52 max-w-full mr-4"/>
-    <div className="text-lg font-serif">
-      {article.layout?.map(layoutEntity => ({
-        text: <SanitizeHTML html={layoutEntity.settings.text} options={{}}/>
-      }[layoutEntity.component as 'text'] as any))}
-    </div>
-  </Site>;
+export default function Article() {
+  return <Site narrow={true} children={[]}/>;
 }
 
 
